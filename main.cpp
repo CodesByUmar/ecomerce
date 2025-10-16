c0b0f4cc06ac3ca720d12bbd7c76c38a6663115c
Muhammad Umar Asatillayev <asatillayevmuhammadumar00@gmail.com>
2025-10-16 19:10:18 +0500

    add

---
+++ main.cpp
@@ -0,0 +1,113 @@
+#include <iostream>
+#include <locale>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL,"");
+   /* 1st proj
+   string toys[5] = {"мишка", "зайчик", "лисичка", "волчок", "белочка"};
+    for (int i = 0; i < 5; i++) {
+        cout << toys[i] << endl;
+    }*/
+
+    /* 2nd proj
+    int shokoladnye = 5;
+    int karamelki = 8;
+    int iriski = 3;
+
+    int total = shokoladnye + karamelki + iriski;
+
+    cout << "Всего конфет у Пети: " << total << endl;
+
+    */
+
+
+
+
+
+
+
+    /*3rd proj
+      const int SIZE = 5;
+    int heights[SIZE] = {120, 125, 118, 130, 122};
+
+    for (int i = 0; i < SIZE; i++) {
+        cout << "Рост друга " << i + 1 << ": " << heights[i] << " см" << endl;
+    }
+
+    */
+
+     /*
+     4th proj
+     char* colors[] = {"красный", "синий", "зеленый", "желтый", "оранжевый"};
+
+
+    int num_colors = 5;
+
+
+    for (int i = 0; i < num_colors; i++) {
+        cout << colors[i] << endl;
+    }
+    */
+
+
+
+    /*
+    5 th prog
+    int grades[] = {4, 5, 3, 4, 5, 2, 4}
+    nt count = 0;
+
+
+    int num_grades = 7;
+     for (int i = 0; i < num_grades; i++) {
+        if (grades[i] == 4) {
+            count++;
+        }
+     }
+     cout << "Количество учеников, получивших оценку '4': " << count << endl;
+    */
+
+    /*
+    //6 th proj
+
+    int prices[5] = {100, 55, 40, 80, 60};
+
+    int total = 0;
+
+
+    for (int i = 0; i < 5; i++) {
+        cout << "Цена продукта " << i + 1 << ": " << prices[i] << " рублей" << endl;
+        total += prices[i];
+    }
+
+    cout << "Общая сумма: " << total << " рублей" << endl;
+
+    */
+    /*
+
+    const int SIZE = 5;
+    string words[SIZE] = {"home", "river", "forest", "mountains", "snow"};
+
+    for (int i = 0; i < SIZE; i++) {
+        cout << "Word: " << words[i]
+             << " — weight: " << words[i].length() << " characters" << endl;
+    }
+
+    return 0;
+
+    */
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
