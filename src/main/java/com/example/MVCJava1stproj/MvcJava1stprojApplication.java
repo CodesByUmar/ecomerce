d77763f61d00c9a1bbfcfc522ea79bc78d0cfa54
CodesByUmar <CodesByUmar@users.noreply.github.com>
2025-10-16 19:14:47 +0500

    Initial commit

---
+++ src/main/java/com/example/MVCJava1stproj/MvcJava1stprojApplication.java
@@ -0,0 +1,13 @@
+package com.example.MVCJava1stproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcJava1stprojApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MvcJava1stprojApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/MVCJava1stproj/MvcJava1stprojApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MVCJava1stproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcJava1stprojApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
